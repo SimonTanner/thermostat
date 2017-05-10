04cb43d765d6145c27554f21cbcc95d12a8c99f4
SimonTanner <simonthetanner@hotmail.com>
2017-05-10 13:10:23 +0100

    Thermostat Up function increases temperature, test written for decreasing temperature

--- spec/ThermostatSpec.js
+++ spec/ThermostatSpec.js
@@ -2,16 +2,24 @@ describe ('Thermostat',function(){
 
   var thermostat;
 
-     beforeEach(function(){
+  beforeEach(function(){
 
-         thermostat = new Thermostat();
+   thermostat = new Thermostat();
 
-     });
+  });
 
-      it('starts at 20 degress',function(){
+  it('starts at 20 degress',function(){
+    expect(thermostat.temperature).toBe(20);
+  });
 
-      expect(thermostat.temperature).toBe(20)
+  it('Up function can increase the temperature', function() {
+    thermostat.UpTemp();
+    expect(thermostat.temperature).toBe(21);
+  });
 
-    });
+  it('Down function can decrease the temperature', function() {
+    thermostat.DownTemp();
+    expect(thermostat.temperature).toBe(19);
+  });
 
 });
