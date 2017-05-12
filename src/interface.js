443c35a6021f10e18df0c021c060f43e020c6f41
SimonTanner <simonthetanner@hotmail.com>
2017-05-12 15:05:13 +0100

    jquery & interface.js added

---
+++ src/interface.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+  console.log(1);
+  var thermostat = new Thermostat();
+  console.log(thermostat);
+  console.log(2);
+  // $('#temperature').text(thermostat.temperature);
+  console.log(3);
+});
